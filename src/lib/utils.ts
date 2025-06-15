bf86f808da25811047c699ca536cc997c4385402
Lukas Schneider <privat.lukas.schneider@gmail.com>
2025-06-15 17:22:44 +0200

    FIX utils error when building

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,29 @@ export type WithoutChild<T> = T extends { child?: any } ? Omit<T, "child"> : T;
 export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, "children"> : T;
 export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
 export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
+
+const DIVISIONS = [
+	{ amount: 60, name: 'seconds' },
+	{ amount: 60, name: 'minutes' },
+	{ amount: 24, name: 'hours' },
+	{ amount: 7, name: 'days' },
+	{ amount: 4.34524, name: 'weeks' },
+	{ amount: 12, name: 'months' },
+	{ amount: Number.POSITIVE_INFINITY, name: 'years' }
+] as const;
+
+const formatter = new Intl.RelativeTimeFormat(undefined, {
+	numeric: 'auto'
+});
+
+export function formatTimeAgo(date: Date) {
+	let duration = (date.getTime() - new Date().getTime()) / 1000;
+
+	for (let i = 0; i <= DIVISIONS.length; i++) {
+		const division = DIVISIONS[i];
+		if (Math.abs(duration) < division.amount) {
+			return formatter.format(Math.round(duration), division.name);
+		}
+		duration /= division.amount;
+	}
+}
