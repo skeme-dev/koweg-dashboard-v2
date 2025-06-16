98353654bbb1e9267752d4e9b225717f628ce756
skeme-dev <101049368+skeme-dev@users.noreply.github.com>
2025-06-16 12:12:20 +0200

    vite.config.ts aktualisieren

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
-	
+	server: {
+			allowedHosts: ["dashboard-v2.koweg.skeme.dev"]
+	},
 	build: {
 		commonjsOptions: {
 			include: [/node_modules/],
