786e13f823595a30b16f99da255a984c4d20ccb6
Lukas Schneider <privat.lukas.schneider@gmail.com>
2025-06-17 22:57:00 +0200

    FIX forbidden error when building

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,19 @@
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import {
+	vitePreprocess
+} from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter(), alias: {"@/*": "./src/lib/*",}, kit: {
-  csrf: {
-    checkOrigin: false
-  }
-} }
+	kit: {
+		adapter: adapter(),
+		alias: {
+			"@/*": "./src/lib/*",
+		},
+			csrf: {
+				checkOrigin: false
+			}
+	}
 };
 
 export default config;
